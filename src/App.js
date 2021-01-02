307203674ae9e78806d9c33be39d93e3a5c51ef7
jakepark@carrotglobal.com <park4023>
2021-01-02 12:48:11 +0900

    라우팅 적용적용

--- src/App.js
+++ src/App.js
@@ -1,52 +1,42 @@
 import React from "react";
-import axios from "axios";
-import Movie from "./Movie";
-import "./App.css"
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import About from "./routes/About";
+import Home from "./routes/Home";
 
-class App extends React.Component {
-  state = {
-    isLoading: true,
-    movies: []
-  };
-  getMovies = async () => {
-    const {
-      data: {
-        data: { movies }
-      }
-    } = await axios.get(
-        "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
-    );
-    this.setState({ movies, isLoading: false });
-  };
-  componentDidMount() {
-    this.getMovies();
-  }
-  render() {
-    const { isLoading, movies } = this.state;
-    return (
-        <section class="container">
-          {isLoading ? (
-              <div class="loader">
-                <span class="loader__text">Loading...</span>
-              </div>
-          ) : (
-              <div class="movies">
-                {movies.map(movie => (
-                    <Movie
-                        key={movie.id}
-                        id={movie.id}
-                        year={movie.year}
-                        title={movie.title}
-                        summary={movie.summary}
-                        poster={movie.medium_cover_image}
-                        genres={movie.genres}
-                    />
-                ))}
-              </div>
-          )}
-        </section>
-    );
-  }
+function App() {
+  return (
+      <Router>
+        <div>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/dashboard">Dashboard</Link>
+            </li>
+          </ul>
+
+          <hr />
+
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/about">
+              <About />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+  );
 }
 
 export default App
